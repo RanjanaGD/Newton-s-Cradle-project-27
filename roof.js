47051f13beedb320e2c1179fc4b6bfcb1eeb08c9
RanjanaGD <69150679+RanjanaGD@users.noreply.github.com>
2020-09-14 16:59:20 +0530

    Update roof.js

--- roof.js
+++ roof.js
@@ -3,15 +3,23 @@ class Roof {
       var options = {
           isStatic: true
       }
-      this.body = Bodies.rectangle(x,y,width,height,options);
+     
       this.width = width;
       this.height = height;
+      this.x=x;
+      this.y=y;
+
+      this.body = Bodies.rectangle(x,y,width,height,options);
+
       World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;
+      push();
+      translate(pos.x,pos.y);
       rectMode(CENTER);
       fill(128);
-      rect(pos.x, pos.y, this.width, this.height);
+      rect(0,0, this.width, this.height);
+      pop();
     }
   };
